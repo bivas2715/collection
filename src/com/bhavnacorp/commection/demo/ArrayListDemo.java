c49f926aad3c1b0198848a5f5769973851d29f43
Bivas Dey <Bdey@BHAVNACORP.NET>
2018-06-30 13:19:23 +0530

    collection demo

---
+++ src/com/bhavnacorp/commection/demo/ArrayListDemo.java
@@ -0,0 +1,73 @@
+package com.bhavnacorp.commection.demo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        System.out.println("-----------------Start-----------");
+
+        //List
+        List list = new ArrayList();
+
+        list.add("Abhi");
+        list.add("raj");
+        list.add("bivas");
+        list.add("himanshi");
+        list.add(3);
+        list.add(3.8);
+
+        System.out.println("----------- Inserted data-----------   "+list);
+        System.out.println("----------Fetching record on the basis of index-----------");
+        System.out.println("2nd index record  : "+list.get(2));
+
+        System.out.println("------ Remove object from list -----");
+
+        //on the basis of index
+        System.out.println("---- remove  2nd index value   " +list.remove(2));
+        System.out.println("----- updated list---  "+list);
+
+        //on the basis of object
+        System.out.println("----- remove on the basis of object ------");
+        System.out.println(list.remove("raj"));
+        System.out.println("---- updated list" +list);
+
+        //Cheak it exist or not
+        System.out.println("Contain : "+list.contains("Abhi"));
+
+
+        //Add  all
+        List list2 = new ArrayList();
+        list2.add("kavita");
+        list2.add("dhaka");
+
+     //   list.addAll(list2);
+        list.addAll(1,list2);
+
+        System.out.println("Updated list -----  "+list);
+
+
+        //Remove all
+      //  list.removeAll(list2);
+        System.out.println("Updated list -----  "+list);
+
+        //Cheak size
+        int size = list.size();
+        System.out.println(size);
+
+        List<String> list3 = new ArrayList<String>();
+        list3.add("kavita");
+        list3.add("abcd");
+     //   list3.add(3);
+
+        System.out.println(list3);
+
+        List<Integer> list4 = new ArrayList<Integer>();
+
+
+
+
+
+    }
+}
---
+++ src/com/bhavnacorp/commection/demo/LinkListDemo.java
@@ -0,0 +1,23 @@
+package com.bhavnacorp.commection.demo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkListDemo {
+    public static void main(String[] args) {
+        List list = new LinkedList();
+        //add 3 object
+        list.add("Delhi");
+        list.add("Pune");
+        list.add("Bangalore");
+
+        System.out.println("---List records---"+list);
+        list.size();
+
+
+
+        //add other lis after specific position
+
+
+    }
+}
